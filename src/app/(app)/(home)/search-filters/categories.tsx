387d034f44a47f206e20239ef9a77cbb2e5eb8fd
Mahesh Moholkar <maheshmoholkar171@gmail.com>
2025-09-04 23:14:16 +0530

    add categories dropdown

---
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -0,0 +1,22 @@
+import { Category } from "@/payload-types";
+import CategoryDropdown from "./category-dropdown";
+
+const Categories = ({ data }: { data: any }) => {
+  return (
+    <div className="relative w-full">
+      <div className="flex flex-nowrap items-center">
+        {data.map((category: Category) => (
+          <div key={category.id}>
+            <CategoryDropdown
+              category={category}
+              isActive={false}
+              isNavigationHovered={false}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Category } from "@/payload-types";
+import React, { useRef, useState } from "react";
+import { useDropdownPosition } from "./use-dropdown-position";
+import SubcategoryMenu from "./subcategory-menu";
+
+const CategoryDropdown = ({
+  category,
+  isActive,
+  isNavigationHovered,
+}: {
+  category: Category;
+  isActive?: boolean;
+  isNavigationHovered?: boolean;
+}) => {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const { getDropdownPosition } = useDropdownPosition(dropdownRef);
+
+  const onMouseEnter = () => {
+    if (category.subcategories) setOpen(true);
+  };
+
+  const onMouseLeave = () => setOpen(false);
+
+  const dropdownPosition = getDropdownPosition();
+
+  return (
+    <div
+      className="relative"
+      ref={dropdownRef}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <div className="relative">
+        <Button
+          variant="elevated"
+          className={cn(
+            "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+            isActive && !isNavigationHovered && "bg-white border-primary"
+          )}
+        >
+          {category.name}
+        </Button>
+        {category.subcategories && category.subcategories.length > 0 && (
+          <div
+            className={cn(
+              "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+              open && "opacity-100"
+            )}
+          />
+        )}
+      </div>
+      <SubcategoryMenu
+        category={category}
+        isOpen={open}
+        position={dropdownPosition}
+      />
+    </div>
+  );
+};
+
+export default CategoryDropdown;
