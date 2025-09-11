db2055dc553cd887e814afa1a87b70f443186f95
Mahesh Moholkar <maheshmoholkar171@gmail.com>
2025-09-11 15:32:52 +0530

    fix filters

--- src/modules/products/searchParams.ts
+++ src/modules/products/searchParams.ts
@@ -1,4 +1,4 @@
-import { parseAsArrayOf, useQueryStates, parseAsString } from "nuqs";
+import { createLoader, parseAsString, parseAsArrayOf } from "nuqs/server";
 
 const params = {
   minPrice: parseAsString.withOptions({ clearOnDefault: true }),
@@ -6,6 +6,4 @@ const params = {
   tags: parseAsArrayOf(parseAsString).withOptions({ clearOnDefault: true }),
 };
 
-export const useProductFilters = () => {
-  return useQueryStates(params);
-};
+export const loadProductFilters = createLoader(params);
